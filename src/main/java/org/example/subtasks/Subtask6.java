93399bd0c7b806c832f0e1a0fb6a7e084a1f18a9
dchabanovskyi <a2881dante.dev@gmail.com>
2022-05-21 22:28:04 +0300

    Build: Add subtask 6
    Задать два стека, поменять информацию местами.

---
+++ src/main/java/org/example/subtasks/Subtask6.java
@@ -0,0 +1,32 @@
+package org.example.subtasks;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * Задать два стека, поменять информацию местами.
+ */
+public class Subtask6 {
+
+    public static void run() {
+        System.out.println("Subtask 6:");
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for(int i=0; i < (int)(Math.random() * 10 + 5); i++) {
+            set1.add((int)(Math.random() * 10));
+        }
+        for(int i=0; i < (int)(Math.random() * 10 + 5); i++) {
+            set2.add((int)(Math.random() * 10));
+        }
+        System.out.println(set1 + " | " + set2);
+
+        Set<Integer> tempSet = new HashSet<>(set1);
+        set1 = new HashSet<>(set2);
+        set2 = new HashSet<>(tempSet);
+        System.out.println(set1 + " | " + set2);
+
+        System.out.println("\n=========================\n");
+    }
+
+}
